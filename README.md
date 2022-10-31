a34e89103f5aceaa4b4f7ba3688dec08b2c9acd8
badrinath948 <117059767+badrinath948@users.noreply.github.com>
2022-10-31 07:52:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# new-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-brjt7v)
---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+content: boolean;
+ send(){
+   if (confirm('send')){
+     alert('yes')
+   }
+   else{
+     alert('no')
+   }
+ }
+
+ getVal(){
+   alert('keyup')
+ }
+ check(event) {
+  if ( event.target.checked ) {
+      this.content = true;
+      alert('selected')
+ } else{
+   alert('unselected')
+ }
+}
+
+radio(event){
+  if(event.target.radio){
+    this.content = true;
+  }else{
+    alert('no')
+  }
+}
+down(){
+  alert('drag')
+}
+scroll(){
+  alert("scrollung")
+}
+
+}
+
+
